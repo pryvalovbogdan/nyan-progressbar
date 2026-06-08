0ea629361298e99472be26bdc2938792c024275c
Bohdan Privalov <bohdan.privalov@tech-stack.io>
2026-06-08 17:47:14 +0300

    updated regex

--- src/proxy.ts
+++ src/proxy.ts
@@ -39,6 +39,6 @@ export function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.gif$|.*\\.svg$|.*\\.ico$|.*\\.png$).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|ads.txt|.*\\.gif$|.*\\.svg$|.*\\.ico$|.*\\.png$).*)',
   ],
 };
